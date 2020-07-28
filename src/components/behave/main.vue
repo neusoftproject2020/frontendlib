9a372fef92fee2c3705e0ff88b9bb9c7bcebe51f
吕海东 <haidonglu@126.com>
2020-07-28 13:20:34 +0800

    吕海东：修改了爱好和员工主组件

--- src/components/behave/main.vue
+++ src/components/behave/main.vue
@@ -3,40 +3,32 @@
 	      <div class="box">
 	        <div class="box-header with-border">
 	          <h3 class="box-title">爱好管理</h3>
-	          <div class="box-tools pull-right">
-	            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-	                    title="Collapse">
-	              <i class="fa fa-minus"></i></button>
-	            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-	              <i class="fa fa-times"></i></button>
-	          </div>
 	        </div>
 	        <div class="box-body">
 	       	<table class="table table-bordered">
 				  <thead>
 				    <tr>
 				      <th scope="col">编号</th>
-				      <th scope="col">编码</th>
 				      <th scope="col">名称</th>
 				      <th scope="col">操作</th>
 				    </tr>
 				  </thead>
 				  <tbody>
-				  <c:forEach items="${departmentList}" var="dm">
+				 
 				    <tr>
-				      <td></td>
-				      <td></td>
-				      <td>${dm.name }</td>
+				      <td>1</td>
+				      <td>旅游</td>
+				     
 				      <td><a href="tomodify.mvc" class="btn btn-default">修改</a>
 				          <a href="todelete.mvc" class="btn btn-danger">删除</a> 
 				          <a href="toview.mvc" class="btn btn-default">查看</a>  </td>
 				    </tr>
-				  </c:forEach> 
+				 
 				  </tbody>
 				</table>
 	        </div>
 	        <!-- /.box-body -->
-	        <a href="toadd.mvc" class="btn btn-default">增加新部门</a>
+	        <a href="toadd.mvc" class="btn btn-default">增加新爱好</a>
 	      </div>
 
 </template>
--- src/components/employee/main.vue
+++ src/components/employee/main.vue
@@ -1,37 +1,51 @@
 <template>
-	<!-- Default box -->
-	      <div class="box">
-	        <div class="box-header with-border">
-	          <h3 class="box-title">员工管理</h3>
-	         
-	        </div>
-	        <div class="box-body">
-	       	<table class="table table-bordered">
-				  <thead>
-				    <tr>
-				      <th scope="col">编号</th>
-				      <th scope="col">编码</th>
-				      <th scope="col">名称</th>
-				      <th scope="col">操作</th>
-				    </tr>
-				  </thead>
-				  <tbody>
-				  <c:forEach items="${departmentList}" var="dm">
-				    <tr>
-				      <td></td>
-				      <td></td>
-				      <td>${dm.name }</td>
-				      <td><a href="tomodify.mvc" class="btn btn-default">修改</a>
-				          <a href="todelete.mvc" class="btn btn-danger">删除</a> 
-				          <a href="toview.mvc" class="btn btn-default">查看</a>  </td>
-				    </tr>
-				  </c:forEach> 
-				  </tbody>
-				</table>
-	        </div>
-	        <!-- /.box-body -->
-	        <a href="toadd.mvc" class="btn btn-default">增加新部门</a>
-	      </div>
+	<div class="box-body">
+		    <table id="EmployeeTable" class="table table-bordered table-hover">
+		      <thead>
+		      <tr>
+		        <th>账号</th>
+		        <th>姓名</th>
+		        <th>性别</th>       
+				<th>年龄</th>
+				<th>工资</th>
+				<th>生日</th>
+				<th>入职日期</th>
+				<th>部门</th>
+				<th>操作</th>
+				</tr>
+		      </thead>
+		      <tbody>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>       
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>修改 删除 查看  </td>
+				</tr>
+			  </tbody>
+		    </table>
+		    <div class="row">
+		    	<div class="col-md-6">
+		    	个数:<span></span>	页数:<span></span>/<span></span>
+		    	</div>
+			    <div class="col-md-6">
+				    <nav>
+					  <ul class="pagination justify-content-end">
+					    <li class="page-item"><a class="page-link" href="#">首页</a></li>
+					    <li class="page-item"><a class="page-link" href="#" >上页</a></li>
+					    <li class="page-item"><a class="page-link" href="#" >下页</a></li>
+					    <li class="page-item"><a class="page-link" href="#" >末页</a></li>
+					  </ul>
+					</nav>
+			    </div>
+			    
+			</div> 
+			 <div class="row" v-if="message!=null">{{message}}</div>  
+	</div>
 
 </template>
 
